e0f2c5dce0b8ac5284e4c7ae40fe6de0d60db2ba
FameofUkraine <7295016@gmail.com>
2019-06-19 16:06:38 +0300

    example

--- example.php
+++ example.php
@@ -10,39 +10,17 @@
 <body>
 
 <?php
-$bmw = array(
-    "model" => "X5",
-    "speed" => 120,
-    "doors" => 5,
-    "year" => "2006"
-);
-$toyota = array(
-    "model" => "Carina",
-    "speed" => 130,
-    "doors" => 4,
-    "year" => "2007"
-);
-$opel = array(
-    "model" => "Corsa",
-    "speed" => 140,
-    "doors" => 5,
-    "year" => "2007"
-);
-$cars = array();
-$cars[] = $bmw;
-$cars[] = $toyota;
-$cars[] = $opel;
 
-echo "<p>BMW - " . $bmw["model"] . " - " . $bmw["speed"] . " - " . $bmw["doors"] . " - " . $bmw["year"] . "</p>";
-echo "<p>TOYOTA - " . $toyota["model"] . " - " . $toyota["speed"], " - " . $toyota["doors"]," - " . $toyota["year"] . "</p>";
-echo "<p>OPEL - " . $opel["model"] . " - " . $opel["speed"] . " - " . $opel["doors"] . " - " . $opel["year"] . "</p>";
-echo "<p>", $cars[0] ["model"], " : ", $cars[0]["year"],
-"</p?>";
-echo "<p>", $cars[1] ["model"], " : ", $cars[1]["year"],
-"</p?>";
-echo "<p>", $cars[2] ["model"], " : ", $cars[2]["year"],
-"</p?>";
-
+function factorial ($x) {
+    if ($x==0) {
+        return 1;
+    }
+    else {
+        return $x * factorial($x-1);
+    }
+}
+$result=factorial(5);
+echo "5! =".$result;
 
 ?>
 </body>
